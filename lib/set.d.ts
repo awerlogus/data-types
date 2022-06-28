bc821faf4647d9341f97246b9cfd54cc34573765
awerlogus <awerlogus@yandex.ru>
2022-06-29 00:24:37 +0300

    Added sets disjoint function

--- lib/set.d.ts
+++ lib/set.d.ts
@@ -72,3 +72,7 @@ export const nonEmpty: B.Predicate<[ReadonlySet<any>]>
 export const intersects: <T>(set1: ReadonlySet<T>, set2: ReadonlySet<T>) => boolean
 
 export const intersectsC: <T>(set1: ReadonlySet<T>) => (set2: ReadonlySet<T>) => boolean
+
+export const disjoint: <T>(set1: ReadonlySet<T>, set2: ReadonlySet<T>) => boolean
+
+export const disjointC: <T>(set1: ReadonlySet<T>) => (set2: ReadonlySet<T>) => boolean
