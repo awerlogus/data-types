d7db61b7094fc85840b054faee1fa09a699d2b7e
awerlogus <awerlogus@yandex.ru>
2022-07-08 03:16:19 +0300

    [Breaking changes] Fixed overlooked map-map keys values entries methods param order

--- lib/map-map.d.ts
+++ lib/map-map.d.ts
@@ -14,11 +14,23 @@ export const getC: <K1, K2>(key1: K1, key2: K2) => <V>(map: ReadonlyMap<K1, Read
 
 export const getCR: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key1: K1, key2: K2) => O.Option<V>
 
-export const keys: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<K2>
+export const keys: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>, key: K1) => Iterable<K2>
 
-export const values: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<V>
+export const keysC: <K1>(key: K1) => <K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => Iterable<K2>
 
-export const entries: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<[K2, V]>
+export const keysCR: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<K2>
+
+export const values: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>, key: K1) => Iterable<V>
+
+export const valuesC: <K1>(key: K1) => <K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => Iterable<V>
+
+export const valuesCR: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<V>
+
+export const entries: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>, key: K1) => Iterable<[K2, V]>
+
+export const entriesC: <K1>(key: K1) => <K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => Iterable<[K2, V]>
+
+export const entriesCR: <K1, K2, V>(map: ReadonlyMap<K1, ReadonlyMap<K2, V>>) => (key: K1) => Iterable<[K2, V]>
 
 export const set: <K1, K2, V>(map: Map<K1, Map<K2, V>>, key1: K1, key2: K2, value: V) => boolean
 
